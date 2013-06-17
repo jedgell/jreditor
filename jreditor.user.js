6e0705b9d700b545e2f09e2ceef9c19d7cff5eb9
Jeremy Edgell <jeremye@knowclassic.com>
2013-06-17 17:08:50 -0400

    Corrected tabbing for readability, added grant privileges to make greasemonkey complain less, changed location on page, made text selectable on click.

--- jreditor.user.js
+++ jreditor.user.js
@@ -1,17 +1,31 @@
 // ==UserScript==
-// @name       Stash message from JIRA
-// @namespace  http://knowclassic.com/
-// @version    0.1-dev
-// @description  Uses ticket information from JIRA to cobble together a basic Stash commit message.
-// @include	   https://jira.*
-// @copyright  2013+, Classic Graphics
+// @name       		Stash message from JIRA
+// @namespace  		http://knowclassic.com/
+// @version    		0.1-dev
+// @description  	Uses ticket information from JIRA to cobble together a basic Stash commit message.
+// @include	   		https://jira.*
+// @grant       	none
+// @copyright  		2013+, Classic Graphics
 // ==/UserScript==
 
-if (jira.app && $('#viewissuesidebar').length() = 1) {
+if (jira.app && $('div.issue-body').length > 0) {
+
 	issue_key = jira.app.issue.getIssueKey();
 	issue_summary = $('#summary-val').text();
 	if ( !issue_summary.match('/\.$/') ) {
 		issue_summary += '.';
 	}
-	$('#viewissuesidebar').append(('<div id="stash-dialog" title="Stash Message" class="module toggle-wrap"><div class="mod-header"><h2 class="toggle-title">Stash Message</h2></div><div class="mod-content">' + issue_key + ': ' + issue_summary + '</div></div>'));
+	bg_color = $('#details-module').css('background-color');
+	font_size = $('#descriptionmodule .user-content-block p').css('font-size');
+	line_height = $('#descriptionmodule .user-content-block p').css('line-height');
+
+	$('#details-module').after('<div id="stash-dialog" title="Stash Message" class="module toggle-wrap"><div class="mod-header"><h2 class="toggle-title">Stash Message</h2></div><div class="mod-content"><input type="text" name="" value="' + issue_key + ': ' + issue_summary + '" style="width:100%; border:0px solid ' + bg_color + '; font-size: ' + font_size + '; + line-height: ' + line_height + '; cursor: hand; cursor: pointer;" /></div></div>');
+
+	$(document).ready(function(){
+		$('#stash-dialog .mod-content input').click(function(){
+			$(this).focus();
+			$(this).select();
+		})
+	});
+
 }
