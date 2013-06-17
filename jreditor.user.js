46c9e71a6c87b1027f50a88aba9e5bb9c6e5928d
Jeremy Edgell <jeremye@knowclassic.com>
2013-06-17 18:07:21 -0400

    Readability cleanup & make sure we're not dealing with multi-issue tickets (Epics)

--- jreditor.user.js
+++ jreditor.user.js
@@ -8,16 +8,19 @@
 // @copyright  		2013+, Classic Graphics
 // ==/UserScript==
 
-if (jira.app && $('div.issue-body').length > 0) {
+var is_issue = $('div.issue-body').length;
+var issue_type = $('#type-val').text().replace(/\s+/g, '');
 
-	issue_key = jira.app.issue.getIssueKey();
-	issue_summary = $('#summary-val').text();
+if (jira.app && is_issue > 0 && issue_type != 'Epic') {
+
+	var issue_key = jira.app.issue.getIssueKey();
+	var issue_summary = $('#summary-val').text();
 	if ( !issue_summary.match('/\.$/') ) {
 		issue_summary += '.';
 	}
-	bg_color = $('#details-module').css('background-color');
-	font_size = $('#descriptionmodule .user-content-block p').css('font-size');
-	line_height = $('#descriptionmodule .user-content-block p').css('line-height');
+	var bg_color = $('#details-module').css('background-color');
+	var font_size = $('#descriptionmodule .user-content-block p').css('font-size');
+	var line_height = $('#descriptionmodule .user-content-block p').css('line-height');
 
 	$('#details-module').after('<div id="stash-dialog" title="Stash Message" class="module toggle-wrap"><div class="mod-header"><h2 class="toggle-title">Stash Message</h2></div><div class="mod-content"><input type="text" name="" value="' + issue_key + ': ' + issue_summary + '" style="width:100%; border:0px solid ' + bg_color + '; font-size: ' + font_size + '; + line-height: ' + line_height + '; cursor: hand; cursor: pointer;" /></div></div>');
 
