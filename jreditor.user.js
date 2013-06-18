39d28e931126d4173244b94bd705e4d77c9ae9a1
Jeremy Edgell <jeremye@knowclassic.com>
2013-06-18 10:29:27 -0400

    Resolves #3 by loading an external jQuery instance and setting a separate jQuery handler function (jQ).

--- jreditor.user.js
+++ jreditor.user.js
@@ -8,27 +8,48 @@
 // @copyright  		2013+, Classic Graphics
 // ==/UserScript==
 
-var is_issue = $('div.issue-body').length;
-var issue_type = $('#type-val').text().replace(/\s+/g, '');
+//From http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
 
-if (jira.app && is_issue > 0 && issue_type != 'Epic') {
+// a function that loads jQuery and calls a callback function when jQuery has finished loading
+function addJQuery(callback) {
+	var script = document.createElement("script");
+	script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+	script.addEventListener('load', function() {
+			var script = document.createElement("script");
+			script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+			document.body.appendChild(script);
+		}, false);
+	document.body.appendChild(script);
+}
 
-	var issue_key = jira.app.issue.getIssueKey();
-	var issue_summary = $('#summary-val').text();
-	if ( !issue_summary.match('/\.$/') ) {
-		issue_summary += '.';
+function main() {
+
+	var is_issue = jQ('div.issue-body').length;
+	var issue_type = jQ('#type-val').text().replace(/\s+/g, '');
+
+	if (jira.app && is_issue > 0 && issue_type != 'Epic') {
+
+		var issue_key = jira.app.issue.getIssueKey();
+		var issue_summary = jQ('#summary-val').text();
+		if ( !issue_summary.match('/\.jQ/') ) {
+			issue_summary += '.';
+		}
+		var bg_color = jQ('#details-module').css('background-color');
+		var font_size = jQ('#descriptionmodule .user-content-block p').css('font-size');
+		var line_height = jQ('#descriptionmodule .user-content-block p').css('line-height');
+
+		jQ('#details-module').after('<div id="commit-dialog" title="Commit Message" class="module toggle-wrap"><div class="mod-header"><h2 class="toggle-title">Commit Message</h2></div><div class="mod-content"><input type="text" name="" value="' + issue_key + ': ' + issue_summary + '" style="width:100%; border:0px solid ' + bg_color + '; font-size: ' + font_size + '; + line-height: ' + line_height + '; cursor: hand; cursor: pointer;" /></div></div>');
+
+		jQ(document).ready(function(){
+			jQ('#commit-dialog .mod-content input').click(function(){
+				jQ(this).focus();
+				jQ(this).select();
+			})
+		});
+
 	}
-	var bg_color = $('#details-module').css('background-color');
-	var font_size = $('#descriptionmodule .user-content-block p').css('font-size');
-	var line_height = $('#descriptionmodule .user-content-block p').css('line-height');
-
-	$('#details-module').after('<div id="commit-dialog" title="Commit Message" class="module toggle-wrap"><div class="mod-header"><h2 class="toggle-title">Commit Message</h2></div><div class="mod-content"><input type="text" name="" value="' + issue_key + ': ' + issue_summary + '" style="width:100%; border:0px solid ' + bg_color + '; font-size: ' + font_size + '; + line-height: ' + line_height + '; cursor: hand; cursor: pointer;" /></div></div>');
-
-	$(document).ready(function(){
-		$('#commit-dialog .mod-content input').click(function(){
-			$(this).focus();
-			$(this).select();
-		})
-	});
 
 }
+
+// load jQuery and execute the main function
+addJQuery(main);
